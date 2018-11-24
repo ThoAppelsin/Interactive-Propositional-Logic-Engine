c145e98f9dc7f1e98a5b0322fa088787f3fe5046
Utkan Gezer <utkangezer@gmail.com>
2018-11-24 08:32:58 +0300

    Added Sequent parsing.

--- julia/src.jl
+++ julia/src.jl
@@ -22,18 +22,19 @@ struct Imp
 end
 
 struct Sequent
-	premises::Array{Formula}
-	conclusion::Formula
+	Φ::Array{Formula}
+	ψ::Formula
 end
 
 @enum Assoc LeftAssoc RightAssoc
 
 PARENS = ["()", "[]"]
 ATOM = r"^[A-Za-z][0-9]*$"
-NEG = (Neg, ["¬", "!", "-"])
-AND = (And, ["∧", "&", "*", "×"])
-OR  = (Or,  ["∧", "|", "+"])
-IMP = (Imp, ["→", "->"])
+NEG = ["!"]
+AND = ["&", "*"]
+OR  = ["|", "+"]
+IMP = ["->"]
+SEQ = ["=>"]
 
 function balanced_parens(ϕ, parens)
 	c = 0
@@ -52,37 +53,33 @@ end
 
 balanced_parens(ϕ) = all(balanced_parens(ϕ, parens) for parens ∈ PARENS)
 
-function parse_unary(ϕ, OP)
-	Op, ops = OP
-	for op ∈ ops
+function parse_unary(ϕ, Op, OP)
+	for op ∈ OP
 		if startswith(ϕ, op)
 			return Op(parse_formula(a[length(op)+1:end]))
 		end
 	end
 end
 
-function parse_binary(ϕ, OP, assoc)
-	Op, ops = OP
-	for op ∈ ops
-		if op ∈ ϕ
-			splits = split(ϕ, op)
-			e_range = (assoc == LeftAssoc ? reverse : identity)(1:length(splits)-1)
-			for e ∈ e_range
-				ϕ₁ = join(splits[1:e], op)
-				ϕ₂ = join(splits[e+1:end], op)
-				if balanced_parens(ϕ₁) && balanced_parens(ϕ₂)
-					return Op(parse_formula(ϕ₁), parse_formula(ϕ₂))
-				end
+function parse_binary(ϕ, Op, OP, assoc)
+	for op ∈ OP
+		splits = split(ϕ, op)
+		e_range = (assoc == LeftAssoc ? reverse : identity)(1:length(splits)-1)
+		for e ∈ e_range
+			ϕ₁ = join(splits[1:e], op)
+			ϕ₂ = join(splits[e+1:end], op)
+			if balanced_parens(ϕ₁) && balanced_parens(ϕ₂)
+				return Op(parse_formula(ϕ₁), parse_formula(ϕ₂))
 			end
 		end
 	end
 end
 
 is_atomic(ϕ) = match(ATOM, ϕ) == ϕ
-parse_neg(ϕ) = parse_unary(ϕ, NEG)
-parse_and(ϕ) = parse_binary(ϕ, AND, LeftAssoc)
-parse_or(ϕ)  = parse_binary(ϕ, OR,  LeftAssoc)
-parse_imp(ϕ) = parse_binary(ϕ, IMP, RightAssoc)
+parse_neg(ϕ) = parse_unary(ϕ, Neg, NEG)
+parse_and(ϕ) = parse_binary(ϕ, And, AND, LeftAssoc)
+parse_or(ϕ)  = parse_binary(ϕ, Or, OR, LeftAssoc)
+parse_imp(ϕ) = parse_binary(ϕ, Imp, IMP, RightAssoc)
 
 function converge(subject, functions)
 	while true
@@ -112,4 +109,21 @@ function parse_formula(ϕ)
 	else
 		println("Either we parsed it wrong or you supplied a malformed formula: >$ϕ<")
 	end
+end
+
+function parse_sequent(Δ)
+	Φ = ""
+	ψ = Δ
+	for seq ∈ SEQ
+		splits = split(Δ, seq)
+		if length(splits) == 2
+			Φ, ψ = splits
+			break
+		end
+		if length(splits) > 2
+			println("Multiple $seq symbols detected")
+		end
+	end
+	Φ = map(strip, split(Φ, ','))
+	Sequent(Φ, ψ)
 end
